7c06cc3108c5087d79a1234a2cc3541c7f685677
Rabeya-Khanom <rabeyakhanom33@gmail.com>
2024-12-02 15:18:58 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+var x=70;
+var y=20;
+var z=x+y;
+console.log(z);
+
+function myFunction(){
+   document.getElementById("demo").innerHTML='hi phd' ;
+   document.getElementById("Demo").style.fontSize = "35px";
+}
+
+function myFunction(name){
+    console.log(" hi" + name);
+}
+myFunction(" rabeya");
+myFunction(" rabeya");
+myFunction(" sumi");
+myFunction(" mamota");
+
+function myFunction(){
+document.getElementById('myImage').src='image/WhatsApp Image 2024-08-18 at 21.56.02_0f673bfd.jpg';
+document.getElementById('myImage').src= "image/c:\Users\WALTON\Documents\WhatsApp Image 2024-07-15 at 15.16.12_eb9d049c.jpg"
+}
+
+function myFunction(){
+    document.getElementById('class').style.display="none";
+}
+
+    function myFunction(){
+   document.getElementById('Rikta').style.display='block';
+}
